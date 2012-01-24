6ed046bfaff7123b87267239a40bb52ddd431afb
Jacob H. Hansen <jacob.hansen@shrt.info>
2012-01-25 00:42:16 +0100

    .

--- ComponentKit.cs
+++ ComponentKit.cs
@@ -11,12 +11,9 @@
 /// 
 /// * The scripting in [Unity3D](http://www.unity3d.com)
 /// * [PushButton Engine V2](http://pushbuttonengine.com)
-/// * [Sterling](http://www.sterlingdatabase.com/) is actually not that similar, but it's still pretty sweet.
 /// 
 /// Though originally intended as an educational reference/example on how to implement *yet another entity system*, it should be perfectly fine for use in real productions too.
 /// 
 /// Further **documentation** can be found through the button at the upper-right corner of this page. All documentation has been generated straight from the source, using [Nocco](http://dontangg.github.com/nocco/).
 /// 
 /// The complete [source for ComponentKit](http://github.com/shrt/ComponentKit) can be found at github. Do with it what you want!
-/// 
-// TODO: project: TOP SECRET - share ideas
